3424ada1363468916681bfa1603390af409cc0e9
Mihan3000 <121653576+Mihan3000@users.noreply.github.com>
2026-04-11 01:16:56 +0400

    Add files via upload

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 #
 #	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2020-2023 The OrangeFox Recovery Project
+# 	Copyright (C) 2020-2021 The OrangeFox Recovery Project
 #
 #	OrangeFox is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
@@ -18,13 +18,14 @@
 # 	Please maintain this if you use this script or any part of it
 #
 FDEVICE="marble"
+#set -o xtrace
 
 fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep -w \"$FDEVICE\")
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
    if [ -n "$chkdev" ]; then 
       FOX_BUILD_DEVICE="$FDEVICE"
    else
-      chkdev=$(set | grep BASH_ARGV | grep -w \"$FDEVICE\")
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
       [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
    fi
 }
@@ -34,16 +35,50 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-	export FOX_AB_DEVICE=1
-    export FOX_USE_BASH_SHELL=1
-    export FOX_USE_SED_BINARY=1
-    export FOX_USE_TAR_BINARY=1
-    export FOX_ENABLE_APP_MANAGER=1
-    export FOX_USE_NANO_EDITOR=1
-    export FOX_VIRTUAL_AB_DEVICE=1
-else
-	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
-		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
-	fi
+	export LC_ALL="C.UTF-8"
+ 	export ALLOW_MISSING_DEPENDENCIES=true
+ 	
+ 	#OFR build settings & info
+ 	export FOX_VANILLA_BUILD=1
+	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+	export TARGET_DEVICE_ALT="marble"
+	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+	export FOX_DELETE_INITD_ADDON=1
+	export FOX_MOVE_MAGISK_INSTALLER_TO_RAMDISK=1
+	export FOX_SETTINGS_ROOT_DIRECTORY="/metadata/OFRP"
+	export FOX_ALLOW_EARLY_SETTINGS_LOAD=1
+	export FOX_MISCELLANEOUS_ROOT_DIRECTORY="/sdcard"
+
+	# Move the Magisk installer/uninstaller ZIP to the ramdisk
+	export FOX_MOVE_MAGISK_INSTALLER_TO_RAMDISK=1
+
+	#KernelSU
+	export FOX_ENABLE_KERNELSU_SUPPORT=1
+
+	#OFR binary files
+	export FOX_REPLACE_BUSYBOX_PS=1
+	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+	export FOX_REPLACE_TOOLBOX_GETPROP=1
+	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_XZ_UTILS=1
+	export FOX_USE_SED_BINARY=1
+	export FOX_USE_NANO_EDITOR=1
+	export FOX_USE_ZSTD_BINARY=1
+	
+	#OTA
+	export FOX_VIRTUAL_AB_DEVICE=1
+	export FOX_DELETE_AROMAFM=1
+	export FOX_ENABLE_APP_MANAGER=1
+
+	lunch twrp_$FDEVICE-eng
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	fi
 fi
 #
